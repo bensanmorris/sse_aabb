c26fd4ad3386116179fabf0268ba5d448675a29f
Ben Morris <bensan.morris@googlemail.com>
2020-03-03 14:03:26 +0000

    WIP

--- main.cpp
+++ main.cpp
@@ -6,6 +6,29 @@ using namespace std;
 
 int main()
 {
+    /*
+    // original non-sse code:
+    // Move the box to the origin and scale it.
+    const Vector3 center   = (a.m_vMin + a.m_vMax) * Vector3(0.5f);
+    const Vector3 localMin = a.m_vMin - center;
+    const Vector3 localMax = a.m_vMax - center;
+
+    // Compute the extents along each world axis.
+    Vector3 aX = mtx[0]*localMin[0];
+    Vector3 bX = mtx[0]*localMax[0];
+    Vector3 aY = mtx[1]*localMin[1];
+    Vector3 bY = mtx[1]*localMax[1];
+    Vector3 aZ = mtx[2]*localMin[2];
+    Vector3 bZ = mtx[2]*localMax[2];
+
+    // Start at the world-space center of the box.
+    b.m_vMin = b.m_vMax = t + mtx[0]*center.x + mtx[1]*center.y + mtx[2]*center.z;
+
+    // Update the final AABB.
+    b.m_vMin += (glm::min( aX, bX ) + glm::min( aY, bY ) + glm::min( aZ, bZ ));
+    b.m_vMax += (glm::max( aX, bX ) + glm::max( aY, bY ) + glm::max( aZ, bZ ));
+    */
+
     struct alignas(16) V4
     {
         float data[4];
